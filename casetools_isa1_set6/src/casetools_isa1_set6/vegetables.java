314b4f14dc8831381105ad1c4220a2ddeab52ee3
Niki218 <nikitacardozo14@gmail.com>
2024-02-10 10:23:16 +0530

    commit

---
+++ casetools_isa1_set6/src/casetools_isa1_set6/vegetables.java
@@ -0,0 +1,18 @@
+package casetools_isa1_set6;
+
+ public class vegetables {
+	    String name;
+		vegetables(String name)
+		{
+			this.name=name;
+		}
+	
+	
+	public static void main(String[]args) {
+		//TODO Auto-generated method stub
+		vegetables spinach=new vegetables("spinach");
+		System.out.println("vegetables is"+spinach.name);
+		
+	}
+	
+}
